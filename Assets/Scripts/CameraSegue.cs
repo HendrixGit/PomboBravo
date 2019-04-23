6f53bf5a0d50d448f76586543cfd1c7c54d7b28e
Bruno <brunosalustiano303@hotmail.com>
2019-04-22 23:13:04 -0300

    Camera segue, morte passaro

---
+++ Assets/Scripts/CameraSegue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraSegue : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform objE, objD, passaro;
+
+    void Update()
+    {
+        if (passaro != null)
+        {
+            Vector3 posCamera = transform.position;
+            posCamera.x = passaro.position.x;//segue a bola
+            posCamera.x = Mathf.Clamp(posCamera.x, objE.position.x, objD.position.x);//limitei ate onde a camera vai
+            transform.position = posCamera;
+        }
+    }
+}
--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -18,6 +18,10 @@ public class Drag : MonoBehaviour
     private Vector2 catapultToBird;
     private Vector3 pointL;
 
+    private SpringJoint2D spring;
+    private Vector2 preVel;
+    private Rigidbody2D passaroRB;
+
     void Start()
     {
         drag = GetComponent<Collider2D>();
@@ -25,11 +29,17 @@ public class Drag : MonoBehaviour
  
         leftCatapultRay = new Ray(lineFront.transform.position, Vector3.zero);
         passaroCol = GetComponent<CircleCollider2D>();
+
+        spring    = GetComponent<SpringJoint2D>();
+        passaroRB = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         LineUpdate();
+        SpringEffect();
+
+        preVel = passaroRB.velocity;
 
         if (Input.touchCount > 0) {
             touch = Input.GetTouch(0);
@@ -49,7 +59,9 @@ public class Drag : MonoBehaviour
                 }
             }
             if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled) {
+                passaroRB.isKinematic = false;
                 clicked = false;
+                MataPassaro();
             }
         }
     }
@@ -69,4 +81,31 @@ public class Drag : MonoBehaviour
         lineFront.SetPosition(1, pointL);
         lineBack.SetPosition(1, pointL);
     }
+
+    void SpringEffect() {
+        if (spring != null) {
+            if (passaroRB.isKinematic == false) {
+                if (preVel.sqrMagnitude > passaroRB.velocity.sqrMagnitude) {//retorno do comprimente do quadrado do vetor
+                    lineFront.enabled = false;
+                    lineBack.enabled  = false;
+                    //Destroy(spring);
+                    spring.enabled = false;
+                    passaroRB.velocity = preVel;//ajuste de velocidade apos a destruicao do spring
+                    
+                }
+            }
+        }
+    }
+
+    void MataPassaro() {
+        if (passaroRB.velocity.magnitude < 0.5f) {
+            StartCoroutine(TempoMorte());
+        }
+    }
+
+    IEnumerator TempoMorte() {
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+    }
+
 }
