5428b63e9808dbc46c32bfc97cf8497f2c785d94
Bruno <brunosalustiano303@hotmail.com>
2019-04-29 21:49:50 -0300

    Passaro veloz e passaro multiplo adicionados

---
+++ Assets/Scripts/Multiplo.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Multiplo : MonoBehaviour
+{
+
+    private Vector3 start;
+    public  Rigidbody2D pass1, pass2, passaroRB, passPrefab;
+    private bool libera = false;
+    public int trava = 0;
+    private Touch touch;
+    private TrailRenderer trail;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        passaroRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //MOuse
+        //if (Input.GetMouseButtonDown(0) && passaroRB.isKinematic == false && trava == 0) {
+        //    libera = true;
+        //    start = transform.position;
+        //    pass1 = Instantiate(passPrefab, new Vector3(start.x, start.y + 0.1f, start.z), Quaternion.identity);
+        //    pass2 = Instantiate(passPrefab, new Vector3(start.x, start.y - 0.1f, start.z), Quaternion.identity);
+        //    trava = 1;
+        //}
+
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Ended && trava < 2 && passaroRB.isKinematic == false)
+            {
+                trava++;
+                if (trava == 2)
+                {
+                    start = transform.position;
+                    pass1 = Instantiate(passPrefab, new Vector3(start.x, start.y + 0.1f, start.z), Quaternion.identity);
+                    pass2 = Instantiate(passPrefab, new Vector3(start.x, start.y - 0.1f, start.z), Quaternion.identity);
+                    libera = true;
+                }
+            }
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        if (libera) {
+            pass1.velocity     = passaroRB.velocity * 1.6f;
+            pass2.velocity     = passaroRB.velocity * 1.1f;
+            passaroRB.velocity = passaroRB.velocity * 1.4f;
+            libera = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Veloz.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Veloz : MonoBehaviour
+{
+    public Rigidbody2D passaroRB;
+    public bool libera = false;
+    public int trava;
+    private Touch touch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        passaroRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        //if (Input.GetMouseButton(0) && passaroRB.isKinematic == false && trava == 0) {
+        //    libera = true;
+        //    trava  = 1;
+        //}
+
+        if (Input.touchCount > 0) {
+            touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Ended && trava < 2 && passaroRB.isKinematic == false) {
+                trava++;
+                if (trava == 2) {
+                    libera = true;
+                }
+            }
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        if (libera == true) {
+            passaroRB.velocity = passaroRB.velocity * 2.5f;
+            libera = false;
+        }
+    }
+
+}
