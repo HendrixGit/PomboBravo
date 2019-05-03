47d848e5324b08e9ebc0c264a1e03aaa43a2546d
Bruno <brunosalustiano303@hotmail.com>
2019-05-02 23:01:21 -0300

    Cena inicial pronta

---
+++ Assets/Scripts/MorteArea.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorteArea : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("pass")){
+            Destroy(col.gameObject);
+        }
+    }
+}
