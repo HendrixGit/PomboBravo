7d0d80fbf2ec2a13155aa36675b5e6d3851ca1da
Bruno <brunosalustiano303@hotmail.com>
2019-05-04 00:47:29 -0300

    Loading adicionado

---
+++ Assets/Scripts/LoadingInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingInfo : MonoBehaviour
+{
+    public Text carregando; 
+
+    public void BtnClick() {
+        StartCoroutine(LoadGameProg());
+    }
+
+    IEnumerator LoadGameProg() {
+        AsyncOperation async = SceneManager.LoadSceneAsync(1);
+        while (!async.isDone) {
+            carregando.enabled = true;
+            yield return null;
+        }
+    }
+}
