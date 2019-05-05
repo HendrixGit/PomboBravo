515e8f5faccfc19f99ef6015506e62487d5dd3d5
Bruno <brunosalustiano303@hotmail.com>
2019-05-04 22:21:55 -0300

    Adicionado link para a play store

---
+++ Assets/Scripts/Baixa_Fut.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Baixa_Fut : MonoBehaviour
+{
+
+    public void BaixaFutebol() {
+        Application.OpenURL("market://details?id=com.WTNCursos.MAXFut");//link aplicativo
+    }
+
+}
---
+++ Assets/Scripts/Control_BTN_Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control_BTN_Menu : MonoBehaviour
+{
+    public Animator btnAnim;
+    private bool chave = false;
+
+    public void EventoCliqueMaisJogos() {
+        chave = !chave;
+        if (chave == true)
+        {
+            btnAnim.Play("BTN_MAISGAMES_ANIM");
+        }
+        else {
+            btnAnim.Play("BTN_MAISGAMES_ANIM_INVERSE");
+        }
+    }
+}
