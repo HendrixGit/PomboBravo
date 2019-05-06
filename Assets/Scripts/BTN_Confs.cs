d04ba1d530a3540e5855b71729db519dd1ca2f1e
Bruno <brunosalustiano303@hotmail.com>
2019-05-05 21:47:20 -0300

    Conf adicionadas

---
+++ Assets/Scripts/BTN_Confs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTN_Confs : MonoBehaviour
+{
+    private bool liga = false;
+    public Animator animaConf, animaEngrenagem;
+
+    public void ClickBTN() {
+        liga = !liga;
+
+        if (liga)
+        {
+            animaConf.Play("Conf_Anim");
+            animaEngrenagem.Play("Anima_Engrenagem");
+        }
+        else {
+            animaConf.Play("Conf_Anim_Reverse");
+            animaEngrenagem.Play("Anima_Engrenagem_Reverse");
+        }
+    }
+}
