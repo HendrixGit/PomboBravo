0d5535e63f25b201b021cf6a80c17e97da9fdd0b
Bruno <brunosalustiano303@hotmail.com>
2019-05-13 22:53:32 -0300

    UI Win adicionada

---
+++ Assets/Scripts/TesteWin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesteWin : MonoBehaviour
+{
+
+    public Animator menu, estrela1, estrela2, estrela3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            menu.Play("MenuWin_Anim");
+            estrela1.Play("Estrela1_Anim");
+            estrela2.Play("Estrela2_Anim");
+            estrela3.Play("Estrela3_Anim");
+        }
+
+    }
+}
